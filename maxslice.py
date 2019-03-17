706722684769793a51482b6b5bec56660dc03281
devforfu <developer.z@outlook.com>
2019-03-17 19:05:22 +0500

    Maxslice examples

--- maxslice.py
+++ maxslice.py
@@ -10,6 +10,26 @@ def slow_max_slice(A):
     return result
 
 
+def quadratic_max_slice(A):
+    n = len(A)
+    result = 0
+    pref = prefix_sum(A)
+    for p in range(n):
+        for q in range(p, n):
+            total = pref[q + 1] - pref[p]
+            result = max(result, total)
+    return result
+
+
+def prefix_sum(A):
+    total = 0
+    pref = [total]
+    for x in A:
+        total += x
+        pref.append(total)
+    return pref
+
+
 def test():
     assert slow_max_slice([5, -7, 3, 5, -2, 4, -1]) == 10
 
